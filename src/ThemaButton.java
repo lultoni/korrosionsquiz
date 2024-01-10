701a0bd2144eb7be5deb7867df9da5ead30ef796
DESKTOP-GHV8SML\eeist <eeistcool@gmail.com>
2024-01-10 12:31:22 +0100

    hope of finishing guessing

--- src/ThemaButton.java
+++ src/ThemaButton.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ThemaButton extends JButton {
 
@@ -7,17 +8,25 @@ public class ThemaButton extends JButton {
     private Color guessButtonCol;
     private Color guessButtonCorrect;
     private Color guessButtonFalse;
-    private boolean wasGuessed;
+    private volatile boolean wasGuessed;
+    private JFrame questionWindow;
+    private final ArrayList<Question> questions;
+    private int questionIndex;
 
     public ThemaButton(Thema thema) {
         this.thema = thema;
+        this.questionWindow = null;
+        this.questions = DBHelper.getQuestions(thema);
+        this.questionIndex = 0;
         init();
     }
 
     private void init() {
         setText(thema.name);
         addActionListener(e -> {
-            createQuestionWindow();
+            System.out.println("In Thema: " + thema.name);
+            questionWindow = createQuestionWindow();
+            questionWindow.setVisible(true);
         });
         guessButtonCol = new Color(171, 171, 171);
         guessButtonCorrect = new Color(170, 255, 95);
@@ -25,7 +34,7 @@ public class ThemaButton extends JButton {
         wasGuessed = false;
     }
 
-    private void createQuestionWindow() {
+    private JFrame createQuestionWindow() {
         JFrame frame = new JFrame(getText());
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
@@ -44,58 +53,63 @@ public class ThemaButton extends JButton {
         nP.add(backButton);
 
         frame.add(nP, BorderLayout.NORTH);
-        frame.add(createQuestionPanel());
-
         frame.setVisible(true);
+        JPanel questionPanel = createQuestionPanel();
+        frame.add(questionPanel);
+        return frame;
     }
 
     private JPanel createQuestionPanel() {
+        System.out.println("In createQuestionPanel");
+        Question question = questions.get(questionIndex);
         JPanel questionPanel =  new JPanel();
-        System.out.println("T: " + thema.name);
-        for(Question question: DBHelper.getQuestions(thema)) {
-            wasGuessed = false;
-            System.out.println("Q: " + question.q);
+        wasGuessed = false;
 
-            JPanel upPanel = new JPanel();
-            JLabel qLabel = new JLabel(question.q);
-            JButton conButton = new JButton("Weiter");
-            conButton.addActionListener(e -> {
-                // TODO implement logic
-                conButton.setBackground((wasGuessed) ? guessButtonCorrect : guessButtonFalse);
-            });
+        JPanel upPanel = new JPanel();
+        JLabel qLabel = new JLabel(question.q);
+        JButton conButton = new JButton("Weiter");
+        conButton.addActionListener(e -> {
+            conButton.setBackground((wasGuessed) ? guessButtonCorrect : guessButtonFalse);
+            questionIndex++;
+            System.out.println("qs" + questions.size());
+            System.out.println("qin" + questionIndex);
+            if (questionIndex >= questions.size()) {
+                questionWindow.dispose();
+            } else {
+                questionWindow = createQuestionWindow();
+            }
 
-            upPanel.setLayout(new BorderLayout());
-            upPanel.add(qLabel);
-            upPanel.add(conButton, BorderLayout.EAST);
+        });
 
-            JPanel dPanel = new JPanel();
-            JButton ab1 = new JButton(question.a1);
-            JButton ab2 = new JButton(question.a2);
-            JButton ab3 = new JButton(question.a3);
-            JButton ab4 = new JButton(question.a4);
+        upPanel.setLayout(new BorderLayout());
+        upPanel.add(qLabel);
+        upPanel.add(conButton, BorderLayout.EAST);
 
-            dPanel.setLayout(new GridLayout());
-            dPanel.add(ab1);
-            dPanel.add(ab2);
-            dPanel.add(ab3);
-            dPanel.add(ab4);
+        JPanel dPanel = new JPanel();
+        JButton ab1 = new JButton(question.a1);
+        JButton ab2 = new JButton(question.a2);
+        JButton ab3 = new JButton(question.a3);
+        JButton ab4 = new JButton(question.a4);
 
-            ab1.addActionListener(e -> guessButtonAction(1, question, ab1, ab2, ab3, ab4));
-            ab2.addActionListener(e -> guessButtonAction(2, question, ab1, ab2, ab3, ab4));
-            ab3.addActionListener(e -> guessButtonAction(3, question, ab1, ab2, ab3, ab4));
-            ab4.addActionListener(e -> guessButtonAction(4, question, ab1, ab2, ab3, ab4));
+        dPanel.setLayout(new GridLayout());
+        dPanel.add(ab1);
+        dPanel.add(ab2);
+        dPanel.add(ab3);
+        dPanel.add(ab4);
 
-            ab1.setBackground(guessButtonCol);
-            ab2.setBackground(guessButtonCol);
-            ab3.setBackground(guessButtonCol);
-            ab4.setBackground(guessButtonCol);
+        ab1.addActionListener(e -> guessButtonAction(1, question, ab1, ab2, ab3, ab4));
+        ab2.addActionListener(e -> guessButtonAction(2, question, ab1, ab2, ab3, ab4));
+        ab3.addActionListener(e -> guessButtonAction(3, question, ab1, ab2, ab3, ab4));
+        ab4.addActionListener(e -> guessButtonAction(4, question, ab1, ab2, ab3, ab4));
 
-            questionPanel.setLayout(new GridLayout(0, 1));
-            questionPanel.add(upPanel);
-            questionPanel.add(dPanel);
+        ab1.setBackground(guessButtonCol);
+        ab2.setBackground(guessButtonCol);
+        ab3.setBackground(guessButtonCol);
+        ab4.setBackground(guessButtonCol);
 
-            // TODO implement going to the next question
-        }
+        questionPanel.setLayout(new GridLayout(0, 1));
+        questionPanel.add(upPanel);
+        questionPanel.add(dPanel);
         return questionPanel;
     }
 
